683873372f1bef3532fa9c8504af5ec6a3da929a
Brian best <brian.brianb64@gmail.com>
2015-04-04 22:39:01 -0300

    Getting Auth going

---
+++ scripts/controllers/auth.js
@@ -0,0 +1,37 @@
+'user strict';
+
+app.controller('AuthController', function($scope,$location, Auth){
+
+
+	$scope.register = function(user){
+		Auth.register(user).then(function(){
+			console.log("Register successfully!");
+			$location.path('/');
+		},function(err){
+			console.log("Error...");
+		});
+	};
+
+	$scope.login = function(user){
+		Auth.login(user).then(function(){
+			console.log("Login successfully!");
+			$location.path('/');
+		},function(err){
+			console.log("Error...");
+		});
+	};
+
+	$scope.changePassword = function(user){
+		Auth.changePassword(user).then(function(){
+			$scope.user.email = '';
+			$scope.user.oldPass = '';
+			$scope.user.newPass = '';
+
+			console.log("Password changed successfully!");
+			
+		},function(err){
+			console.log("Error...");
+		});
+	}
+
+});
